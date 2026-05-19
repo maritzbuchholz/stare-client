c76167fe2d88d6b4d933495c513dbb9739e8e471
maritzbuchholz <maritzbuchholz@gmail.com>
2026-05-18 20:20:30 -0400

    finish up animation on hero banner

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,6 @@ import "./Hero.scss";
 const Hero = () => {
     return (
         <header className = "hero">
-            <div className = "hero__buffer"></div>
             <div className = "hero__logo">
                 <img src={StareLogoTransparent} alt="Stare Logo" />
             </div> 
