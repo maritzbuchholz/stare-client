f8f906e3a3505c01a9e46bca907b32fef27dcd74
maritzbuchholz <maritzbuchholz@gmail.com>
2026-05-24 12:03:07 -0400

    start draft of burger menu, using use states

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,29 @@
-import { NavLink } from "react-router-dom";
-import StareLogo from "../../assets/logo/stareLogo.png";
+import { useState } from "react";
+import StareLogoTrasparent from "../../assets/logo/stareLogoTransparent.png";
 import Cart from "../../assets/icons/cart.svg";
 import "./NavBar.scss";
 
 
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <header className = "nav">
             <div className = "nav__logo">
-                <img src={StareLogo} alt="Stare Logo" />
-            </div> 
+                <img src={StareLogoTrasparent} alt="Stare Logo" />
+            </div>
+            
+            <button class="nav__burger" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </button>
 
-            <nav className = "nav__tabs">
+            <nav className={`nav__tabs ${menuOpen ? "nav__tabs--open" : ""}`}>
                 <a className='nav__link' href="#about">About</a>
                 <a className='nav__link' href="#tour">Tour</a>
                 <a className='nav__link' href="#merch">Merch</a>
             </nav>
+
+
 
             <div className = "nav__cart">
                 <img src={Cart} alt="Shopping Cart" />
