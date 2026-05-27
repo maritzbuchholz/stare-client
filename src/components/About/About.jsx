fe6c0f885ad658f4234fd771dd02e485a02220ce
maritzbuchholz <maritzbuchholz@gmail.com>
2026-05-26 20:57:10 -0400

    further update about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,9 @@ const About = () => {
         <div className="about">
             <h1 className="about__title">About</h1>
             <p className="about__description">Two brothers. Low-fi. Raw.</p>
+            <p className="about__description">A project that started in the depths of COVID,
+                <b className="about__name"> Stare</b> aims to strike unease in the heart of it's listeners.</p>
+            <p className="about__description">Unforgiving and merciless.</p>
         </div>
     );
 };
