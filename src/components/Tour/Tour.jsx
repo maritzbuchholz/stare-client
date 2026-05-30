d85c6e7fb821c8c04971ab1b8a45fef538a33875
maritzbuchholz <maritzbuchholz@gmail.com>
2026-05-30 12:07:30 -0400

    include Tour title

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -5,6 +5,7 @@ import Event from "../Event/Event.jsx"
 const Tour = () => {
     return (
         <div className = "tour">
+            <h1 className = "tour__title">Tour</h1>
             <Event />
         </div>
     );
