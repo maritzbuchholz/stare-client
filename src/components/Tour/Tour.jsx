fdf0575f07d2250050357937ba5faf0a896f57b3
maritzbuchholz <maritzbuchholz@gmail.com>
2026-05-30 11:43:46 -0400

    set up tour section structure

---
+++ src/components/Tour/Tour.jsx
@@ -0,0 +1,12 @@
+import "./Tour.scss";
+
+
+const Tour = () => {
+    return (
+        <div className = "tour">
+        </div>
+    );
+
+};
+
+export default Tour;
---
+++ src/components/TourDate/TourDate.jsx
@@ -0,0 +1,13 @@
+import "./TourDate.scss";
+
+
+const TourDate = () => {
+    return (
+        <div className = "tour-date">
+            
+        </div>
+    );
+
+};
+
+export default TourDate;
