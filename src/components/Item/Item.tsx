d5fa33403ad2eb28ccdde69d0acd3a7ce8370417
maritzbuchholz <maritzbuchholz@gmail.com>
2026-07-06 18:32:11 -0400

    update item URL and some sass

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -11,23 +11,23 @@ const Item = ({product}: ItemProps) => {
     const n = 30;
     const quantityLimit = [...Array(n + 1).keys()];
     const sizeArray = product.variants.map((variant) => variant.size);
+    
     return (
         <section className = "item">
-            <img className = "item__picture" src={Placeholder} alt="Placeholder item photo" />
+            <img className = "item__picture" src={product.image_url} alt="Placeholder item photo" />
             <h3 className = "item__description">{product.name}</h3>
             <div className = "item__size-section">
                 { sizeArray.length > 1 ?
-                <>
-                    <label className = "item__label" htmlFor="item__size">Size</label>
-                    <select id="size" name="size">
-                        {sizeArray.map((size) => <option value={size}>{size}</option>)}
-                    </select>
-                </>
+                    <>
+                        <label className = "item__label" htmlFor="item__size">Size</label>
+                        <select id="size" name="size">
+                            {sizeArray.map((size) => <option key={size} value={size}>{size}</option>)}
+                        </select>
+                    </>
                 : null}
             </div>
             <div className = "item__quantity-section">
                 <label className = "item__label">Quantity</label>
-                <div className="item__quantity-controls">
                     <select id="quantity" name="quantity">
                         {quantityLimit.map((i) => (
                             <option key={i} value={i}>
@@ -35,7 +35,6 @@ const Item = ({product}: ItemProps) => {
                             </option>
                         ))}
                     </select>
-                </div>
             </div>
             <Button text="Add to Cart" classname="item__button" />
         </section>
