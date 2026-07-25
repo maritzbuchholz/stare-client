e38439637ed110e0dcf7e948caf9b9b9fa156909
maritzbuchholz <maritzbuchholz@gmail.com>
2026-07-25 15:24:31 -0400

    add shell cartitem

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,13 @@
+import "./CartItem.scss";
+import Button from "../Button/Button"
+
+const CartItem = () => {
+    return (
+        <section>
+            
+        </section>
+    );
+
+};
+
+export default CartItem;
