03b76a068054a8af0ab1a91f7fb1fc523f9dffb7
maritzbuchholz <maritzbuchholz@gmail.com>
2026-07-26 15:51:31 -0400

    build prop mapping for CartItems and Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -13,6 +13,10 @@ const Cart = () => {
             {cart.map((item) => (
                 <CartItem
                 key={item.variant.sku}
+                name={item.product.name}
+                price_cents={item.product.price_cents}
+                quantity={item.quantity}
+                image_url={item.product.image_url}
                 />))}
         </section>
     );
--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,21 +1,29 @@
 import "./CartItem.scss";
 import Button from "../Button/Button"
-import Placeholder from "../../assets/placeholder.png";
+// import Placeholder from "../../assets/placeholder.png";
 import { CartItemType } from "../../types/cartType";
 
-const CartItem = () => {
+type CartItemProps = {
+    key: string;
+    name: string;
+    price_cents: number;
+    quantity: number;
+    image_url: string;
+}
+
+const CartItem = ({key, name, price_cents, quantity, image_url}: CartItemProps) => {
     return (
         <section className = "cart-item">
-            <img className = "cart-item__thumbnail" src={Placeholder}/>
+            <img className = "cart-item__thumbnail" src={image_url}/>
             <section className = "cart-item__left">
                 <div className = "cart-item__upper">
-                    <h2 className = "cart-item__title">Beanie</h2>
-                    <h3 className = "cart-item__price">$15</h3>
+                    <h2 className = "cart-item__title">{name}</h2>
+                    <h3 className = "cart-item__price">${(price_cents / 100).toFixed(2)}</h3>
                 </div>
                 <div className = "cart-item__lower">
                     <div className = "cart-item__user">    
                         <Button className = "cart-item__button" text="-"></Button>
-                        <span className = "cart-item__quantity">6</span>
+                        <span className = "cart-item__quantity">{quantity}</span>
                         <Button className = "cart-item__button" text="+"></Button>
                     </div>
                     <Button className = "cart-item__button" text="Delete"></Button>
