8dc778d70339e574b95adfa461f6eaeae0c7062d
maritzbuchholz <maritzbuchholz@gmail.com>
2026-07-26 13:29:40 -0400

    add text and buttons to cartItem

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -7,13 +7,22 @@ import Placeholder from "../../assets/placeholder.png";
 const CartItem = () => {
     return (
         <section className = "cart-item">
-            <div className = "cart-item__left">
-                <img src={Placeholder}/>
-            </div>
-            <div className = "cart-item__right">
+            <img src={Placeholder}/>
+            <section className = "cart-item__left">
                 <h3 className = "cart-item__title">Beanie</h3>
-                <h2></h2>
-            </div>
+                <h2 className = "cart-item__price">$15</h2>
+                <div className = "cart-item__lower">
+                    <div>    
+                        <button>-</button>
+                        <span>6</span>
+                        <button>+</button>
+                    </div>
+                    <button>delete</button>
+                </div>
+            </section>
+            <section className = "cart-item__right">
+                <h2 className = "cart-item__subtotal">Subtotal: $30</h2>
+            </section>
         </section>
     );
 
