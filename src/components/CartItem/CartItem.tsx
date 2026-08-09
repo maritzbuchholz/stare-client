e94d2d1cbdb02980eb8e6fd6bc227c20939b5edc
maritzbuchholz <maritzbuchholz@gmail.com>
2026-08-09 11:22:25 -0400

    additional cart styling

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -58,10 +58,10 @@ const CartItem = ({
             <section className = "cart-item__left">
                 <div className = "cart-item__upper">
                     <h2 className = "cart-item__title">{name}</h2>
-                    <h3 className = "cart-item__price">${(price_cents / 100).toFixed(2)}</h3>
+                    <h3 className = "cart-item__price">Price: ${(price_cents / 100).toFixed(2)}</h3>
                     {size === "None" ? null:
                         <>
-                            <h3 className = "cart-item__size">{size}</h3>
+                            <h3 className = "cart-item__size">Size: {size}</h3>
                         </>
                     }
                 </div>
