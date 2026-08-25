a4b0a7243e91c47f21b714511c00db335155f204
maritzbuchholz <maritzbuchholz@gmail.com>
2026-08-24 20:03:46 -0400

    (Claude) Update trash icon for cart to a standalone svg

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,6 +1,7 @@
 import "./CartItem.scss";
 import { Dispatch, SetStateAction, useContext } from "react";
 import CartContext from "../../context/CartContext";
+import Trash from "../../assets/icons/trash.svg";
 
 const MAX_QUANTITY = 10;
 
@@ -73,24 +74,7 @@ const CartItem = ({
                         onClick={onDelete}
                         aria-label={`Remove ${name} from cart`}
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="20"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            aria-hidden="true"
-                        >
-                            <path d="M4 7l16 0" />
-                            <path d="M10 11l0 6" />
-                            <path d="M14 11l0 6" />
-                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                        </svg>
+                        <img src={Trash} alt="" className="cart-item__delete-icon" />
                     </button>
                 </div>
                 <div className="cart-item__footer">
