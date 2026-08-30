4d23a055e81a5f7aaa6d08604e47a0264bbacccd
maritzbuchholz <maritzbuchholz@gmail.com>
2026-08-30 14:44:48 -0400

    update merch section for readability

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
             <h1 className="about__title">About</h1>
             <p className="about__description">Two brothers. Low-fi. Raw.</p>
             <p className="about__description">A project that started in the depths of COVID,
-                <b className="about__name"> Stare</b> aims to strike unease in the heart of it's listeners.</p>
+                <b className="about__name"> Stare</b> aims to strike unease in the heart of its listeners.</p>
             <p className="about__description">Unforgiving and merciless.</p>
             <a className = "about__insta-link" href="#">
                 <img className = "about__insta" src={Insta} alt="Instragram icon" />
