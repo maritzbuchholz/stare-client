6f9d03a8681d0521bdd1c2851d97222334f4f407
maritzbuchholz <maritzbuchholz@gmail.com>
2026-09-05 10:15:09 -0400

    update font size and spacing for About component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="about" id="about">
             <h1 className="about__title">About</h1>
             <p className="about__description">Two brothers. Low-fi. Raw.</p>
-            <p className="about__description">A project that started in the depths of COVID,
+            <p className="about__description">A project born in the depths of COVID,
                 <b className="about__name"> Stare</b> aims to strike unease in the heart of its listeners.</p>
             <p className="about__description">Unforgiving and merciless.</p>
             <a className = "about__insta-link" href="#">
